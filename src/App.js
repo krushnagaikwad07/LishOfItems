84bb329c321ebceafb1653d76e5229c79aef31bd
krushnagaikwad07 <krushnagaikwad7799@gmail.com>
2023-03-14 22:12:30 +0530

    List of Items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+    const [text, setText] = useState([]);
+    const [newText, setnewText] = useState("");
+    const [count, setcount] = useState(0);
 
-export default App;
+    function handleAdd() {
+        if (newText !== "") {
+            const newtext = [...text, { elem: newText, completed: false }];
+            setText(newtext);
+            setnewText("");
+            setcount(count + 1);
+        }
+    };
+
+    function handleonDelete(index) {
+        const newtext = [...text];
+        if (!newtext[index].completed) {
+            setcount(count - 1);
+        }
+        newtext.splice(index, 1);
+        setText(newtext);
+    };
+
+    const handleonComplete = (index) => {
+        const newtext = [...text];
+        console.log(newtext[index]);
+        if (newtext[index].completed === false) {
+            newtext[index].completed = true
+            setText(newtext);
+            count > 0 && setcount(count - 1);
+        }
+        setText(newtext);
+        console.log(newtext)
+    };
+    return (
+        <div className="App">
+            <div className="outer-main">
+                <div className="main-box">
+                    <div>
+                        <div> List Of Items ({count})</div>
+                        {text.map((elem, index) => (
+                            <div key={index}>
+                                {elem.completed ? (
+                                   <s>{elem.elem}</s>
+                                ) : (
+                                    elem.elem
+                                )}
+                                <button onClick={() => handleonComplete(index)}>Complete</button>
+                                <button onClick={() => handleonDelete(index)}>Delete</button>
+                            </div>
+                        ))}
+                    </div>
+                    <input
+                        type="text"
+                        value={newText}
+                        onChange={(e) => setnewText(e.target.value)}
+                    />
+                    <button onClick={handleAdd}>Add</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
